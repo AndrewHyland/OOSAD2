20a2666770363dabeb24506a580eae25f927fa0e
AndrewHyland <x00091348@ittd.ie>
2015-02-13 13:25:48 +0000

    added week 3 work

---
+++ week3/labweek3/Shopping/Cart.java
@@ -0,0 +1,14 @@
+package labweek3.Shopping;
+
+import java.util.*;
+
+public class Cart {
+
+	protected Collection<CartItem> cartItems;
+
+	public Cart() {
+		// TODO - implement Cart.Cart
+		
+	}
+
+}
---
+++ week3/labweek3/Shopping/CartItem.java
@@ -0,0 +1,10 @@
+package labweek3.Shopping;
+
+public class CartItem {
+
+	public CartItem() {
+		// TODO - implement CartItem.CartItem
+		throw new UnsupportedOperationException();
+	}
+
+}
