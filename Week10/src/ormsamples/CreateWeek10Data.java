b6fc669951dd6f8de24d4f5fccc4c43ad824fdbd
AndrewHyland <x00091348@ittd.ie>
2015-04-17 13:45:00 +0100

    Some java, mysql edits

--- Week10/src/ormsamples/CreateWeek10Data.java
+++ Week10/src/ormsamples/CreateWeek10Data.java
@@ -5,13 +5,35 @@
 package ormsamples;
 
 import org.orm.*;
+import java.util.Scanner;
+
 public class CreateWeek10Data {
 	public void createTestData() throws PersistentException {
-		PersistentTransaction t = ormtest.Week10PersistentManager.instance().getSession().beginTransaction();
+        Scanner sc = new Scanner(System.in);		
+        PersistentTransaction t = ormtest.Week10PersistentManager.instance().getSession().beginTransaction();
 		try {
 			ormtest.Employee lormtestEmployee = ormtest.Employee.createEmployee();
 			// Initialize the properties of the persistent object here
-			lormtestEmployee.save();
+			lormtestEmployee.setName("A Hyland");
+                        lormtestEmployee.setAddress("123 Fake Street");
+                        lormtestEmployee.setPps("123456");
+                        lormtestEmployee.setMobile("014445656");
+                        int recs = 2;
+                      //for(int i = 0; i < recs.length; i++){
+                          System.out.println("Please enter your name: ");
+                          lormtestEmployee.setName(sc.next());
+                          sc.nextLine();
+                          System.out.println("Please enter your address: ");
+                          lormtestEmployee.setAddress(sc.next());
+                          sc.nextLine();
+                          System.out.println("Please enter your pps no: ");
+                          lormtestEmployee.setPps(sc.next());
+                          sc.nextLine();
+                          System.out.println("Please enter your mobile no: ");
+                          lormtestEmployee.setMobile(sc.next());
+                          sc.nextLine();  
+                      // }
+                        lormtestEmployee.save();
 			t.commit();
 		}
 		catch (Exception e) {
